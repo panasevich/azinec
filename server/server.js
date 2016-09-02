9e155da4988e60a86f4c5ef6224334b50c908f37
Alexey Panasevich <panasevich.alex@gmail.com>
2016-09-02 22:54:47 +0300

    add CORS

--- server/server.js
+++ server/server.js
@@ -15,6 +15,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 RoutesConfig.init(app);
 DBConfig.init();
 Routes.init(app, express.Router());
